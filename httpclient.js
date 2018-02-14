97b286eaf4dd96dcaec0e7b73bc06379b6a7cb17
Anthony-Salvatori <36006363+Anthony-Salvatori@users.noreply.github.com>
2018-02-14 00:48:50 -0500

    Update httpclient.js

--- httpclient.js
+++ httpclient.js
@@ -1,15 +1,42 @@
 var http = require("http");
+var bl = require('bl');
+var jata = [];
+var countResult = 0;
 
-http.get(process.argv[2], callback);
+for (var i = 2; i < process.argv.length; ++i)
+{
+    
+    http.get(process.argv[i], callback);
+}
 
 function callback(response)
 {
     response.setEncoding("utf8");
-    response.on("data", function (data)
+    
+    response.pipe(bl(function ( err, data) 
     {
-        
-        
-        console.log(data);
-        
-    });
+        if (err)
+        {
+            throw err;
+        }
+        else 
+        {
+            jata[countResult] = data.toString;
+            countResult++;
+            printResult(jata, countResult);
+            
+            
+        }
+    }));
 }
+
+function printResult (data, countResult)
+{
+    if (countResult == 3)
+    {
+        data.forEach(function (data){
+           console.log(data); 
+        });
+    }
+}
+
