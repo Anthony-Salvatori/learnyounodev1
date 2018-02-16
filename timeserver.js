3fe28aa5149f1483a683acf811a0d2eee255dc14
Anthony-Salvatori <36006363+Anthony-Salvatori@users.noreply.github.com>
2018-02-15 23:24:44 -0500

    Update timeserver.js

--- timeserver.js
+++ timeserver.js
@@ -1,33 +1,12 @@
-var net = require('net'); 
-var date = new Date();
-var year = date.getFullYear();
-var month = date.getMonth() + 1;
-if (month < 10)
-{
-    month = "0" + month;
-}
-var day = date.getDate();
-if (day < 10)
-{
-    day = "0" + day;
-}
-var hour = date.getHours();
-if (hour < 10)
-{
-    hour = "0" + hour;
-}
-var min = date.getMinutes();
-if (min < 10)
-{
-    min = "0" + min;
-}
+var fs = require('fs');
+var map = require('through2-map');
 
-var realDate = (year + "-" + month + "-" + day + " " + hour + ":" + min + "\n");
-
-var server = net.createServer(function (socket) 
+var http = require('http'); 
+var server = http.createServer(function (req, res) 
     {  
-       // socket handling logic  
-       socket.write(realDate);
-       socket.end();
-     }) ; 
-server.listen(process.argv[2]); 
+       // request handling logic... 
+      req.pipe(map (function (chunk){
+          return chunk.toString().toUpperCase();
+      })).pipe(res);
+     });  
+server.listen(Number(process.argv[2]));  
